d97be73c382bdd4d41835fe46f175d1ac616442a
Dalton <eschberger.dalton@gmail.com>
2019-11-11 14:52:06 -0600

    setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import 'bootstrap'
 import './App.css';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+
+    render() {
+
+        return (
+            <div className="App">
+                <Form>
+                    <Form.Label>Purchase Price</Form.Label>
+                    <Form.Control placeholder="Enter Price"/>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+            </div>
+        );
+    }
 }
+
 
 export default App;
